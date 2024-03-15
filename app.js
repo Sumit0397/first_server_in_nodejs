b7dd17d48d15f94e7fc03b27de9c7cdbfe5971f2
Sumit0397 <sumitmajumdar16@gmail.com>
2024-03-15 11:24:30 +0530

    different url

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const http = require("http");
 
 
 const server = http.createServer((request , response) => {
-    console.log("sumit");
-    response.end("sumit");
+    const url = request.url;
+    if(url === "/home"){
+        response.write("<html>");
+        response.write("<head><title>Home Page</title></head>");
+        response.write("<body><h1>Welcome Home</h1></body>");
+        response.write("</html>");
+        response.end();    
+    }
+    if(url === "/about"){
+        response.write("<html>");
+        response.write("<head><title>About Page</title></head>");
+        response.write("<body><h1>Welcome to About Us Page</h1></body>");
+        response.write("</html>");
+        response.end();    
+    }
+    if(url === "/node"){
+        response.write("<html>");
+        response.write("<head><title>Node Page</title></head>");
+        response.write("<body><h1>Welcome to my Node Js project</h1></body>");
+        response.write("</html>");
+        response.end();    
+    }
+
 })
 
 server.listen(4000);
